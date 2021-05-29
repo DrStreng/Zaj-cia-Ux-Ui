1002ef63921b16766b7cfb09ee65faf9610a072a
D.Streng <strengol1@gmail.com>
2021-05-29 16:26:17 +0200

    Tabele,CSS,Forms,struct,HTML5 inlinve vs block

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,22 @@ app.use(express.static('../frontend'))
 
 // GET localhost:8080/
 app.get('/', (req, res) => {
-    // const name = req.query.name || 'Guest'
-    // res.send('<h1>Hello ' + name + '</h1>');
     res.sendFile('index.html', { root: '../frontend/' })
 });
 
 app.get('/onas', (req, res) => {
-    res.sendFile('onas.html', { root: '../frontend/' })
+    res.sendFile('onas.html', { root: '../frontend/onas/' })
+});
+
+app.get('/elementy', (req, res) => {
+    res.sendFile('elementy.html', { root: '../frontend/html5/' })
+});
+
+app.get('/struktura', (req, res) => {
+    res.sendFile('struktura.html', { root: '../frontend/html5/' })
+});
+app.get('/tabele', (req, res) => {
+    res.sendFile('tabele.html', { root: '../frontend/html5/' })
 });
 
 app.get('/api', (req, res) => {
@@ -25,3 +34,4 @@ const port = 80; // Default HTTP WWW Port
 app.listen(port, 'localhost', () => {
     console.log('Server listening on http://localhost:' + port + '/')
 });
+
